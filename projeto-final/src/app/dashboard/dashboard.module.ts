afab2c94dbc7bdfb57fd0b4c60ba65fa3298ebdf
Gilson Silva <gilsonsilvati@gmail.com>
2020-03-02 22:22:11 -0300

    Criando as rotas do módulo Dashboard.

--- projeto-final/src/app/dashboard/dashboard.module.ts
+++ projeto-final/src/app/dashboard/dashboard.module.ts
@@ -3,13 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardComponent } from './dashboard.component';
 import { DadosService } from './dados.service';
+import { DashboardRoutingComponent } from './dashboard-routing.component';
+import { DashboardRoutingModule } from './dashboard-routing.module';
 
 @NgModule({
   declarations: [ 
-    DashboardComponent
+    DashboardComponent,
+    DashboardRoutingComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    DashboardRoutingModule
   ],
   exports: [
     DashboardComponent
