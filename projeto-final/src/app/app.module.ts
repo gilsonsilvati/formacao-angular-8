1a302b5d587536d283e7746da02822399be8fa3c
Gilson Silva <gilsonsilvati@gmail.com>
2020-03-03 21:04:13 -0300

    Integrando e criando rotas para o Gerenciador de tarefas.

--- projeto-final/src/app/app.module.ts
+++ projeto-final/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { DashboardModule } from './dashboard';
 import { CalculadoraModule } from './calculadora';
 import { ConversorModule } from './conversor';
+import { TarefasModule } from './tarefas';
 
 @NgModule({
   declarations: [
@@ -18,6 +19,7 @@ import { ConversorModule } from './conversor';
     DashboardModule,
     CalculadoraModule,
     ConversorModule,
+    TarefasModule,
     
     AppRoutingModule
   ],
