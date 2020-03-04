788ceff729571d000ff89d24fa35388f8a0019bf
Gilson Silva <gilsonsilvati@gmail.com>
2020-03-03 21:45:33 -0300

    Integrando e criando rotas para jogo da velha.

--- projeto-final/src/app/app.module.ts
+++ projeto-final/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { DashboardModule } from './dashboard';
 import { CalculadoraModule } from './calculadora';
 import { ConversorModule } from './conversor';
 import { TarefasModule } from './tarefas';
+import { JogoDaVelhaModule } from './jogo-da-velha';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,7 @@ import { TarefasModule } from './tarefas';
     CalculadoraModule,
     ConversorModule,
     TarefasModule,
-    
+    JogoDaVelhaModule,
     AppRoutingModule
   ],
   providers: [],
